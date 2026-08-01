9ad90a218dd95d5c6a7f674c85fa00441013068b
kumari_mahi <mahimthkumari43@gmail.com>
2026-08-01 13:13:54 +0000

    java Leap Year Program

---
+++ leapYear.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class leapYear{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter Year");
+        int year = sc.nextInt();
+        
+        if(year % 400 == 0){
+            System.out.println(year + " is a Leap Year");
+        }else if(year % 100 == 0){
+            System.out.println(year + " is not a Leap Year");
+        }else if(year % 4 == 0){
+            System.out.println(year + " is a Leap Year");
+        }else{
+            System.out.println(year + " is not a Leap Year");
+        }
+        
+    }
+}
